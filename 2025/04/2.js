a87517d5e5e52581b708491e911b7f038f17be80
Bence Uto <uto.bence@gmail.com>
2025-12-04 08:08:02 +0100

    2025/04

---
+++ 2025/04/2.js
@@ -0,0 +1,58 @@
+const fs = require('node:fs');
+
+fs.readFile('input.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    let grid = [];
+    let maxX = 0;
+    let maxY = 0;
+
+    const lines = data.trim().split('\n');
+    for (const line of lines) {
+        grid.push(line.split(''));
+        maxX = line.length;
+    }
+    maxY = grid.length;
+
+    let sum = 0;
+    let foundAny = true;
+    while (foundAny) {
+        foundAny = false;
+        for (let y = 0; y < maxY; y++) {
+            outer: for (let x = 0; x < maxX; x++) {
+                const current = grid[y][x];
+                if (current !== '@') {
+                    continue;
+                }
+
+                let aroundCound = 0;
+                for (let dy = -1; dy <= 1; dy++) {
+                    for (let dx = -1; dx <= 1; dx++) {
+                        if (dx === 0 && dy === 0) {
+                            continue;
+                        }
+                        if (x + dx < 0 || x + dx >= maxX || y + dy < 0 || y + dy >= maxY) {
+                            continue;
+                        }
+
+                        if (grid[y + dy][x + dx] === '@') {
+                            aroundCound++;
+                        }
+
+                        if (aroundCound >= 4) {
+                            continue outer;
+                        }
+                    }
+                }
+                sum++;
+                grid[y][x] = '0';
+                foundAny = true;
+            }
+        }
+    }
+
+    console.log(sum);
+});
